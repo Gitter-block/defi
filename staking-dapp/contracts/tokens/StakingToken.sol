d16794d771a0d7dc8603d21201d37f7166c9423e
Gitter-block <aaron.ong@zoho.com>
2024-12-26 10:32:18 +0800

    Update StakingToken.sol

--- staking-dapp/contracts/tokens/StakingToken.sol
+++ staking-dapp/contracts/tokens/StakingToken.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract StakingToken is ERC20, Ownable {
+    constructor() ERC20("Staking Token", "STK") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
 
